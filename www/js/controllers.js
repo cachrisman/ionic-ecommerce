e24944c068968a830d52b45cdf76b4955a042966
Charlie Chrisman <charlie.chrisman@gmail.com>
2015-07-14 09:29:46 -0700

    added LoginCtrl and AuthService to handle logins

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,6 @@
 angular.module('ionic-ecommerce.controllers', [])
 
-.controller('HomeCtrl', function($scope) {})
+.controller('HomeCtrl', function( $scope) {})
 
 .controller('ProductsCtrl', function($scope, Products) {
   // With the new view caching in Ionic, Controllers are only called
@@ -33,23 +33,41 @@ angular.module('ionic-ecommerce.controllers', [])
 
 .controller('CartCtrl', function($scope, $stateParams, Products) {})
 
-.controller('AccountCtrl', function($scope, UserService) {})
+.controller('AccountCtrl', function($scope, $state, AuthService) {
+  $scope.$on('$ionicView.enter', function(e) {
+    $scope.logged_in = AuthService.isAuthenticated();
+    console.log("logged in: ", $scope.logged_in ? "true" : "false" );
+    $scope.token = AuthService.token();
+    console.log("$scope.token: ", $scope.token);
+  });
 
-.controller('LoginCtrl', function($scope, $state, $ionicPopup, UserService, LoginService) {
-  $scope.user = {};
-  $scope.login = function() {
-    LoginService.loginUser($scope.user.email, $scope.user.password)
+  $scope.logout = function(){
+    AuthService.logout();
+    $scope.user = {};
+    $scope.token = null;
+    $scope.logged_in = AuthService.isAuthenticated();
+    // $state.go('tab.account', {}, {reload: true});
+    $state.reload('tab.account');
+  };
+})
+
+.controller('LoginCtrl', function($scope, $state, $ionicPopup, AuthService) {
+  $scope.login = function(user) {
+    AuthService.login(user.email, user.password)
       .success(function(data) {
-        console.log(data);
-        $state.go('tab.account');
+        $scope.user = {};
+        // $scope.logged_in = AuthService.isAuthenticated();
+        // $scope.token = data.token;
+        console.log("successful login; token: " + data.token);
+        $state.go('tab.account', {}, {reload: true});
+        // $state.reload('tab.account');
       })
       .error(function(data) {
-        console.log(data);
+        console.log("login failed", data.error);
         var alertPopup = $ionicPopup.alert({
           title: 'Login failed!',
-          template: 'Please check your credentials!'
+          template: data.error
         });
       });
   };
-
 });
--- www/js/services.js
+++ www/js/services.js
@@ -1,15 +1,49 @@
 angular.module('ionic-ecommerce.services', [])
 
-.factory('LoginService', function($http, $q) {
-  var LoginService = {};
-  var endpoint = "http://localhost:3000/api/login.json";
-  LoginService.loginUser = function(email, password) {
+.service('AuthService', function($q, $http) {
+
+  var LOCAL_TOKEN_KEY = 'ionic-ecommerce-api-key';
+  var isAuthenticated = false;
+  var authToken;
+
+  function loadUserCredentials() {
+    var token = window.localStorage.getItem(LOCAL_TOKEN_KEY);
+    if (token) {
+      useCredentials(token);
+    }
+  }
+
+  function storeUserCredentials(token) {
+    window.localStorage.setItem(LOCAL_TOKEN_KEY, token);
+    useCredentials(token);
+  }
+
+  function useCredentials(token) {
+    isAuthenticated = true;
+    authToken = token;
+
+    // Set the token as header for your requests!
+    $http.defaults.headers.common['X-Auth-Token'] = token;
+  }
+
+  function destroyUserCredentials() {
+    authToken = undefined;
+    isAuthenticated = false;
+    $http.defaults.headers.common['X-Auth-Token'] = undefined;
+    window.localStorage.removeItem(LOCAL_TOKEN_KEY);
+  }
+
+  var login = function(email, password) {
+    var endpoint = "http://localhost:3000/api/login.json";
     var deferred = $q.defer();
     var promise = deferred.promise;
     var params = { email: email, password: password };
     $http
       .post(endpoint, params)
-      .success(function(response) { deferred.resolve(response); })
+      .success(function(response) {
+        storeUserCredentials(response.token);
+        deferred.resolve(response);
+      })
       .error(function(rejection) { deferred.reject(rejection); });
 
     promise.success = function (fn) {
@@ -22,29 +56,31 @@ angular.module('ionic-ecommerce.services', [])
     };
     return promise;
   };
-  return LoginService;
+
+  var logout = function() {
+    destroyUserCredentials();
+  };
+
+  loadUserCredentials();
+
+  return {
+    login: login,
+    logout: logout,
+    token: function() {return authToken;},
+    isAuthenticated: function() {return isAuthenticated;}
+  };
 })
 
-.factory('UserService', function($window) {
-  var UserService = {};
-  UserService.set = function(token) {
-    $window.sessionStorage.token = token;
-  };
-  UserService.current_user = function() {
-    return $window.sessionStorage.token;
-  };
-  return UserService;
-})
-
-.factory('Products', function($http, $q, UserService) {
+.factory('Products', function($http, $q, AuthService) {
   var endpoint = "http://localhost:3000/api/products";
-  var ProductService = {};
-  ProductService.all = function() {
+  var Products = {};
+  Products.all = function() {
+    // console.log("Products.all()", AuthService.token);
     var deferred = $q.defer();
     $http
       .get(endpoint, {
         headers: {
-          "X-Spree-Token": UserService.current_user()
+          "X-Spree-Token": AuthService.token
         }
       })
       .success(function(response) {
@@ -55,12 +91,12 @@ angular.module('ionic-ecommerce.services', [])
       });
     return deferred.promise;
   };
-  ProductService.get = function(id) {
+  Products.get = function(id) {
     var deferred = $q.defer();
     $http
       .get(endpoint + "/" + id, {
-        params: {
-          token: UserService.current_user().token
+        headers: {
+          "X-Spree-Token": AuthService.token
         }
       })
       .success(function(response) {
@@ -71,5 +107,5 @@ angular.module('ionic-ecommerce.services', [])
       });
     return deferred.promise;
   };
-  return ProductService;
+  return Products;
 });
