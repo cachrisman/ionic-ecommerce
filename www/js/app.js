f972b3150a2e6fd38d012ffb5d81e40e14d411ef
Charlie Chrisman <charlie.chrisman@gmail.com>
2015-07-14 19:10:41 -0700

    refactored app.js with controllerAs

--- www/js/app.js
+++ www/js/app.js
@@ -1,4 +1,9 @@
-var Runner = function($ionicPlatform) {
+angular.module('ionic-ecommerce', ['ionic', 'ionic-ecommerce.controllers', 'ionic-ecommerce.services'])
+.run(Runner)
+.config(Configurator);
+
+Runner.$inject = ['$ionicPlatform'];
+function Runner($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
@@ -10,38 +15,29 @@ var Runner = function($ionicPlatform) {
       StatusBar.styleLightContent();
     }
   });
-};
+}
 
-Runner.$inject = ['$ionicPlatform'];
-
-var Configurator = function($httpProvider, $stateProvider, $urlRouterProvider) {
+Configurator.$inject = ['$httpProvider', '$stateProvider', '$urlRouterProvider'];
+function Configurator($httpProvider, $stateProvider, $urlRouterProvider) {
 
   $httpProvider.defaults.withCredentials = true;
   $httpProvider.defaults.headers.common["X-CSRF-Token"] = $("meta[name=csrf-token]").attr("content");
-
   $httpProvider.defaults.headers.post['Content-Type'] = 'application/json';
 
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
   $stateProvider
 
-  // setup an abstract state for the tabs directive
   .state('tab', {
     url: "/tab",
     abstract: true,
     templateUrl: "templates/tabs.html"
   })
 
-  // Each tab has its own nav history stack:
-
   .state('tab.home', {
     url: '/home',
     views: {
       'tab-home': {
         templateUrl: 'templates/tab-home.html',
-        controller: 'HomeCtrl'
+        controller: 'HomeCtrl as home'
       }
     }
   })
@@ -51,7 +47,7 @@ var Configurator = function($httpProvider, $stateProvider, $urlRouterProvider) {
     views: {
       'tab-products': {
         templateUrl: 'templates/tab-products.html',
-        controller: 'ProductsCtrl'
+        controller: 'ProductsCtrl as products'
       }
     }
   })
@@ -61,7 +57,7 @@ var Configurator = function($httpProvider, $stateProvider, $urlRouterProvider) {
     views: {
       'tab-products': {
         templateUrl: 'templates/product-detail.html',
-        controller: 'ProductDetailCtrl'
+        controller: 'ProductDetailCtrl as product'
       }
     }
   })
@@ -71,7 +67,7 @@ var Configurator = function($httpProvider, $stateProvider, $urlRouterProvider) {
     views: {
       'tab-cart': {
         templateUrl: 'templates/tab-cart.html',
-        controller: 'CartCtrl'
+        controller: 'CartCtrl as cart'
       }
     }
   })
@@ -81,7 +77,7 @@ var Configurator = function($httpProvider, $stateProvider, $urlRouterProvider) {
     views: {
       'tab-account': {
         templateUrl: 'templates/tab-account.html',
-        controller: 'AccountCtrl'
+        controller: 'AccountCtrl as account'
       }
     }
   })
@@ -91,16 +87,10 @@ var Configurator = function($httpProvider, $stateProvider, $urlRouterProvider) {
     views: {
       'tab-account': {
         templateUrl: 'templates/tab-login.html',
-        controller: 'LoginCtrl'
+        controller: 'LoginCtrl as login'
       }
     }
   });
 
-  // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/tab/home');
-};
-
-Configurator.$inject = ['$httpProvider', '$stateProvider', '$urlRouterProvider'];
-
-angular.module('ionic-ecommerce', ['ionic', 'ionic-ecommerce.controllers', 'ionic-ecommerce.services'])
-.run(Runner).config(Configurator);
+}
