0c4f056b2534c12c3adb2fec3accbd3cbe772aea
Charlie Chrisman <charlie.chrisman@gmail.com>
2015-07-14 19:25:01 -0700

    refactoring of controllers for use with ControllerAs

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,68 +1,97 @@
-// Home Controller
-var HomeCtrl = function($scope) {};
+angular.module('ionic-ecommerce.controllers', [])
+.controller('HomeCtrl', HomeCtrl)
+.controller('ProductsCtrl', ProductsCtrl)
+.controller('ProductDetailCtrl', ProductDetailCtrl)
+.controller('CartCtrl', CartCtrl)
+.controller('AccountCtrl', AccountCtrl)
+.controller('LoginCtrl', LoginCtrl);
 
-HomeCtrl.$inject = ['$scope'];
+// Home Controller
+HomeCtrl.$inject = [];
+function HomeCtrl() {}
 
 //Products Controller
-var ProductsCtrl = function($scope, ProductService) {
-  $scope.products = function () {
-    ProductService.all()
-    .then(function(response) {
-        return response.products;
-      },
-      function(rejection) {
-        console.log("ProductsCtrl Products.all error: " + rejection.error);
-      });
-  };
-};
-
 ProductsCtrl.$inject = ['$scope', 'ProductService'];
+function ProductsCtrl($scope, ProductService) {
+  var vm = this;
+  ProductService.all()
+  .then(function(response) {
+      vm.all = response.products;
+    },
+    function(rejection) {
+      console.log("ProductsCtrl Products.all error: " + rejection.error);
+    });
+}
 
 // Product Detail Controller
-var ProductDetailCtrl = function($scope, $stateParams, ProductService) {
+ProductDetailCtrl.$inject = ['$scope', '$stateParams', 'ProductService', 'CartService'];
+function ProductDetailCtrl($scope, $stateParams, ProductService, CartService) {
+  var vm = this;
   var id = $stateParams.productId;
-  $scope.product = function(id) {
-    ProductService.get(id).then(
-      function(response) {
-        return response;
-      },
-      function(rejection) {
-        console.log("ProductDetailCtrl Products.get error: " + rejection.error);
-      })();
-  };
-};
+  vm.add = add;
 
-ProductDetailCtrl.$inject = ['$scope', '$stateParams', 'ProductService'];
+  ProductService.get(id)
+  .then(function(response) {
+      vm.name = response.name;
+      vm.price = response.price;
+      vm.description = response.description;
+      vm.image = response.master.images[0].product_url;
+      vm.product = response;
+      console.log(vm.product);
+    },
+    function(rejection) {
+      console.log("ProductDetailCtrl Products.get error: " + rejection.error);
+    });
+
+  function add(product) {
+    CartService.add(product);
+  }
+}
 
 // Cart Controller
-var CartCtrl = function($scope, $stateParams) {};
+CartCtrl.$inject = ['$scope', '$stateParams', 'CartService'];
+function CartCtrl($scope, $stateParams, CartService) {
+  var vm = this;
+  vm.cart = CartService.products;
+  vm.remove = remove;
 
-CartCtrl.$inject = ['$scope', '$stateParams'];
+  function remove(product) {
+    CartService.remove(product);
+  }
+}
+
 
 // Account Controller
-var AccountCtrl = function($scope, $state, AuthService) {
+AccountCtrl.$inject = ['$scope', '$state', 'AuthService'];
+function AccountCtrl($scope, $state, AuthService) {
+  var vm = this;
+  vm.logout = logout;
+
   $scope.$on('$ionicView.enter', function(e) {
-    $scope.logged_in = AuthService.isAuthenticated();
-    $scope.token = AuthService.token();
+    vm.logged_in = AuthService.isAuthenticated();
+    vm.token = AuthService.token();
   });
 
-  $scope.logout = function(){
+  function logout() {
     AuthService.logout();
-    $scope.user = {};
-    $scope.token = null;
-    $scope.logged_in = AuthService.isAuthenticated();
+    vm.user = null;
+    vm.token = null;
+    vm.logged_in = AuthService.isAuthenticated();
     $state.reload('tab.account');
-  };
-};
-
-AccountCtrl.$inject = ['$scope', '$state', 'AuthService'];
+  }
+}
 
 // Login Controller
-var LoginCtrl = function($scope, $state, $ionicPopup, AuthService) {
-  $scope.login = function(user) {
+LoginCtrl.$inject = ['$scope', '$state', '$ionicPopup', 'AuthService'];
+function LoginCtrl($scope, $state, $ionicPopup, AuthService) {
+  var vm = this;
+  vm.go = go;
+
+  function go(user) {
+    vm.user = null;
+    console.log("hit login function");
     AuthService.login(user.email, user.password)
       .success(function(data) {
-        $scope.user = {};
         console.log("successful login; token: " + data.token);
         $state.go('tab.account', {}, {reload: true});
       })
@@ -73,15 +102,9 @@ var LoginCtrl = function($scope, $state, $ionicPopup, AuthService) {
           template: data.error
         });
       });
-  };
-};
+  }
+}
 
-LoginCtrl.$inject = ['$scope', '$state', '$ionicPopup', 'AuthService'];
 
-angular.module('ionic-ecommerce.controllers', [])
-.controller('HomeCtrl', HomeCtrl)
-.controller('ProductsCtrl', ProductsCtrl)
-.controller('ProductDetailCtrl', ProductDetailCtrl)
-.controller('CartCtrl', CartCtrl)
-.controller('AccountCtrl', AccountCtrl)
-.controller('LoginCtrl', LoginCtrl);
+
+
